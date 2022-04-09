52fb270b78d05ed7c848579bee473765e9e65952
mangalverma <mangal9685963297@gmail.com>
2022-04-09 21:08:59 +0530

    more removed

--- angel_urls.py
+++ angel_urls.py
@@ -4,11 +4,11 @@ remove_attribute = {'https://angelnumber.org': {'id': ['M381992ScriptRootC303780
                     'https://www.coolastro.com/':{'id': ['comments','ez-toc-container'], 'class': ['entry-meta','relpost-thumb-wrapper']},
                     'https://divineangelnumbers.com/':{'id':['comments', 'grow-me-in-content-recs-root'],'class': ['uagb-toc__wrap', 'entry-category-icon', 'entry-meta clear', 'social-share-icons', 'has-background','author-box clear', 'entry-related clear', 'wp-block-image']},
                     'https://www.guardian-angel-reading.com/': {'id': ['share'],'class': ['related-articles fixedfont','homeform homeform--og ', 'byline author vcard','homefooter__socialbuttons homefooter__socialbuttons--posts fixedfont']},
-                    'https://hiddennumerology.com/': {'id': ['ez-toc-container', 'dpsp-content-bottom', 'grow-me-in-content-recs-root'], 'class':['purplebox','pinkbox','yellowbox', 'emalsubinpt', 'saboxplugin-tab', 'dpsp-share-text ', 'navigation post-navigation', 'tablelinks']},
+                    'https://hiddennumerology.com/': {'id': ['ez-toc-container', 'dpsp-content-bottom', 'grow-me-in-content-recs-root','comments'], 'class':['purplebox','pinkbox','yellowbox', 'emalsubinpt', 'saboxplugin-tab', 'dpsp-share-text ', 'navigation post-navigation', 'tablelinks','entry-meta']},
                     'https://www.adviseastro.com' : {'id':['ez-toc-container'],'class':['entry-meta']},
                     'https://www.ipublishing.co.in/' : {'id':['toc_container'],'class':['entry-meta']},
                     'https://joynumber.com/' : {'id':['toc_container'],'class':['css-ujfvsi eods1cv1','css-v5rwlu eods1cv8','css-nq5slz e16ntblo5','css-75up63 e1nq2w9512', 'css-19p6q85 ecyq0i81']}, #problem in 777
-                    'https://mattbeech.com/' : {'id':['toc_container', 'single-comments'],'class':['breadcrumb']},  #need replace_code
+                    'https://mattbeech.com/' : {'id':['toc_container', 'single-comments','main-nav','Other_Repeating_Numbers'],'class':['breadcrumb','nav'],'data-type':['URL','page']},  #need replace_code
                     'https://www.mindbodygreen.com/' : {'class':['subcategory-label c-dark-purple ', 'editor-info__text', 'article-subnavigation', 'ad__callout container', 'article-header__share-like article-header__share-like--standard  article-header__share-like--p2', 'article-image article-image--p1 article-image--hero article-image--hero-standard ', 'article-affiliate-disclaimer', 'article-date article-date--standard', 'article-header__share-like-wrapper', 'article-header__share-like-wrapper article-header__share-like-wrapper--alt']},
                     'https://www.mindfulnessandjustice.org/' : {'class':['saboxplugin-wrap', 'entry-meta', 'InlineRecs___StyledDiv3-sc-7gga64-8 kMYAEH', 'code-block code-block-1']},
                     'https://trustedpsychicmediums.com':{'class':['entry-meta','irp','entry-author author-bottom','tag-cloud','entry-related-posts'],'itemprop':['articleSection'],'id':['toc_container','respond','entry-actions','entry-related-posts']}, #24
@@ -22,9 +22,9 @@ remove_attribute = {'https://angelnumber.org': {'id': ['M381992ScriptRootC303780
                     'https://researchmaniacs.com/': {'id': ['177976545'],'class':['header', 'n-square', 'angel-realted-posts']},
                     'https://www.sunsigns.org/': {'id':[''], 'class': ['single-post-video', 'post-meta', 'share-post']},
                     'https://www.mindyourbodysoul.com': {'id':['comment-count', 'cls-video-container-wqZUnNP4', 'mv-trellis-comments', 'dpsp-content-top'], 'class': ['lwptoc_i', 'entry-footer', 'share-post']},
-                    'https://willowsoul.com/': {'id': ['comments'], 'class': ['remove-element', 'clearfix', 'table-wrapper', 'social-sharing normal']},
+                    'https://willowsoul.com/': {'id': ['comments'], 'class': ['remove-element', 'clearfix', 'table-wrapper', 'social-sharing normal','author']},
                     'https://www.yummymummykitchen.com/': {'id': ['dpsp-content-top','dpsp-content-bottom','grow-me-inline-subscribe-cta-root-0'], 'class': ['post-meta post-date', 'post-meta', 'post-meta meta-centered'], 'href': ['http://amazon.com/Angel-Numbers-101-Meaning-Sequences/dp/1401920012','http://amazon.com/gp/product/1788173473']},
-                    'https://www.thepubliclive.com/': {'class': ['head angel-number', 'post-bottom clear-block', 'story-fb-twitter clear-block']},
+                    'https://www.thepubliclive.com/': {'class': ['head angel-number', 'post-bottom clear-block', 'story-fb-twitter clear-block','author']},
                     'https://angelnumbersmeaning.com': {'id': ['M319879ScriptRootC214660_03376'],'class':['ezoic-ad nativedouble-1 nativedouble-1110 adtester-container adtester-container-110', 'relpost-thumb-wrapper', 'ez-report-ad-button','meta-category']},
                     'https://thesecretofthetarot.com/': {'id': ['ez-toc-container'], 'class': ['entry-header', 'p1', 'rp4wp-related-posts']},
                     'https://takanta.com/': {'id':['ez-toc-container'],'class': ['entry-meta']},
@@ -78,7 +78,7 @@ ignore_tag_by_domain = {'https://angelmanifest.com': ['ul'],
                         'https://www.coolastro.com/': ['ul']}
 page_wise_delimeter = '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
 para_wise_delim = '@@@@@@@@@@@@@@@@@@@'
-ignore_site_num = [1,2,3,4,5,6,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+ignore_site_num = [1,2,3,4,5,6,7,8,9,10,11,12,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 url_patterns = {'1':  ["https://www.adviseastro.com/angel-number-@#$-meaning-and-symbolism/"],
         '2':  ['https://angelmanifest.com/angel-number-@#$/'],
         '3':  ["https://angelnumber.me/@#$-meaning/"],    #no remove attribute need to add for this page
@@ -116,14 +116,15 @@ remove_code = {'https://hiddennumerology.com':[{'orig':'<h3>Read the meaning of 
                'https://angelmanifest.com':[{'orig': '<h3>Other Angel Numbers:</h3>','new':''},
                                             {'orig': '<p>Also read:</p>','new': ''},
                                             {'orig': '<h2>Also Read:</h2>', 'new':''}],
-               'https://numerologycolumn.com/':[{'orig':'<h3 class="h2">Recent Posts</h3>', 'new': ''}],
+               'https://numerologycolumn.com/':[{'orig':'<h3 class="h2">Recent Posts</h3>', 'new': ''},
+                                                {'orig':'<h2>Related posts</h2>', 'new': ''}],
                'http://sacredscribesangelnumbers.blogspot.com': [{'orig':'<span ', 'new': '<p '},
                                                                  {'orig':'</span', 'new': '</p '}],
-               'https://numerologycolumn.com/':[{'orig':'<h2>Related posts</h2>', 'new': ''}],
                'https://researchmaniacs.com/':[{'orig': '<br/><br/>','new':'</p><p>'},
                                             {'orig': '</br>','new': '<p>'},
                                             {'orig': '<b>', 'new':'<h4>'},
-                                            {'orig': '</b>', 'new':'</h4>'}],
+                                            {'orig': '</b>', 'new':'</h4>'},
+                                               {'orig':'You can look up another Angel Number here:','new':''}   ],
                'https://www.sunsigns.org': [{'orig': '<strong>See Also:</strong>','new':''}],
                'https://willowsoul.com': [{'orig': '</header>\n\n        <p>', 'new': '</header>\n\n        <p class = "remove-element">'},
                                           {'orig': "<p><span>PUBLISHER", 'new': "<p class = 'remove-element'><span>PUBLISHER"}],
