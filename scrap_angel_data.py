dabc63092db2b7aa6aceee0a69fa33650df192c3
Kishan-Sahu <sahukishan06@gmail.com>
2022-04-16 13:22:23 +0530

    Final changes

--- scrap_angel_data.py
+++ scrap_angel_data.py
@@ -198,7 +198,7 @@ def main():
     func = partial(scrape_and_parse_data, url_patterns)
     with ProcessPoolExecutor() as executor:
 
-        angel_args = (i for i in range(3362,3363))
+        angel_args = (i for i in range(999,1000))
         executor.map(func,angel_args)
     t_end=time.time()-t
     print(f"{t_end//60} minutes {t_end%60} seconds")
